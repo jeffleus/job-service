5bc9959ee77ae18a15e8be59a1f95b4882f67604
Jeff Leininger <jeffleus@gmail.com>
2017-03-10 13:29:23 -0800

    update for hello/goodbye
    added goodbye function and configured for region, IAMrole, and the vpc
    configuration

--- handler.js
+++ handler.js
@@ -14,3 +14,15 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.goodbye = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Goodbye for Now! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
